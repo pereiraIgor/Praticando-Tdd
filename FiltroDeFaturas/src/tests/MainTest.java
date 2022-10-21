f8eca70f6ff38b12fc9d5dec14b96d2e227ef2c6
Nicolas Moreira Nobre Leite <nicolas.leite@ccc.ufcg.edu.br>
2022-10-21 19:34:03 -0300

    Testando se o JUnit está funcionando corretamente

---
+++ FiltroDeFaturas/src/tests/MainTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void testExample(){
+        assertTrue(true);
+    }
+
+}
