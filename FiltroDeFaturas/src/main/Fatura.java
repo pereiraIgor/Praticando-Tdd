c50f6a2e8c0c297cac76aa67829024183e8311c5
Nicolas Moreira Nobre Leite <nicolas.leite@ccc.ufcg.edu.br>
2022-10-21 19:43:11 -0300

    Corrigindo teste codigo e criando novo teste para verificar valor

--- FiltroDeFaturas/src/main/Fatura.java
+++ FiltroDeFaturas/src/main/Fatura.java
@@ -3,11 +3,15 @@ package main;
 public class Fatura {
     private int codigo;
 
-    public Fatura(int codigo) {
+    public Fatura(int codigo, float valor) {
         this.codigo = codigo;
     }
 
     public int getCodigo() {
+        return this.codigo;
+    }
+
+    public float getValorFatura() {
         return 0;
     }
 }
--- FiltroDeFaturas/src/tests/FaturaTest.java
+++ FiltroDeFaturas/src/tests/FaturaTest.java
@@ -9,7 +9,13 @@ public class FaturaTest {
 
     @Test
     public void testFaturaContemCodigo(){
-        Fatura f = new Fatura(1);
+        Fatura f = new Fatura(1, 1000);
         assertEquals(1, f.getCodigo());
+    }
+
+    @Test
+    public void testFaturaContemValor() {
+        Fatura f = new Fatura(1, 1000);
+        assertEquals(1000, f.getValorFatura());
     }
 }
