08f6ca9b8830ef2e6442407e151d069fd2eebd4e
Igor Pereira <igor.pereira@ccc.ufcg.edu.br>
2022-10-22 15:19:07 -0300

    primeiro teste enviado

---
+++ CalculadoraSalarios/CalculadoraSalarios/src/Test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
+public class Test {
+   @org.junit.Test
+   public void test(){
+        // nome, e-mail, salário-base e cargo.
+        Funcionario func = new Funcionario("Igor", "igo.per@",2000.0, "DBA")
+   }
+}
