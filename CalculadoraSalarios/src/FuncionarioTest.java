150284e596030ee9a398ba923009de74af8324a3
Igor Pereira <igor.pereira@ccc.ufcg.edu.br>
2022-10-22 16:05:09 -0300

    Criando testes para validar difentes funcionarios

--- CalculadoraSalarios/src/FuncionarioTest.java
+++ CalculadoraSalarios/src/FuncionarioTest.java
@@ -22,15 +22,29 @@ public class FuncionarioTest {
       assertEquals("func.1@teste.com", func.getEmail());
    }
    @Test
+   public void testFuncPossuiEmailDiferente(){
+      Funcionario func2 = new Funcionario("", "func.2@teste2.com", 0, "");
+      assertEquals("func.2@teste2.com", func2.getEmail());
+   }
+   @Test
    public void testFuncPossuiSalarioBase(){
       Funcionario func = new Funcionario("funcionario1", "func.1@teste.com", 2000.0, "");
       assertEquals(2000, func.getSalarioBase(), 0);
    }
-
+   @Test
+   public void testFuncPossuiSalarioBaseDiferente(){
+      Funcionario func2 = new Funcionario("funcionario1", "func.1@teste.com", 3500.5, "");
+      assertEquals(3500.5, func2.getSalarioBase(), 0);
+   }
    @Test
    public void testFuncPossuiCargo(){
       Funcionario func = new Funcionario("funcionario1", "func.1@teste.com", 2000.0, "DBA");
       assertEquals("DBA", func.getCargo());
+   }
+   @Test
+   public void testFuncPossuiCargoDiferente(){
+      Funcionario func2 = new Funcionario("funcionario1", "func.1@teste.com", 2000.0, "DESENVOLVEDOR");
+      assertEquals("DESENVOLVEDOR", func2.getCargo());
    }
 
 }
