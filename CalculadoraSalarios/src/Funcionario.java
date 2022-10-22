9f8df3132d311e0bd665ea36986a3c5861a5330a
Igor Pereira <igor.pereira@ccc.ufcg.edu.br>
2022-10-22 15:33:06 -0300

    Criando classe funcionario com atributos

---
+++ CalculadoraSalarios/src/Funcionario.java
@@ -0,0 +1,9 @@
+public class Funcionario {
+    String nome;
+    String email;
+    float salárioBase ;
+    String cargo;
+    public Funcionario(String nome, String email, double salárioBase ,String cargo){
+
+    }
+}
