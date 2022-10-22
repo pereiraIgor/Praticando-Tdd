7a73f280f1b739c3718cc279d9997d323e35e991
Igor Pereira <igor.pereira@ccc.ufcg.edu.br>
2022-10-22 15:40:23 -0300

    Testando instancia diferente

--- CalculadoraSalarios/src/FuncionarioTest.java
+++ CalculadoraSalarios/src/FuncionarioTest.java
@@ -9,4 +9,11 @@ public class FuncionarioTest {
         Funcionario func = new Funcionario("Igor", "igo.per@",2000.0, "DBA");
         assertEquals("Igor", func.getNome());
    }
+
+   @Test
+   public void testFuncNomeDiferente(){
+      Funcionario func2 = new Funcionario("Funcionario","", 0, null);
+      assertEquals("Funcionario", func2.getNome());
+   }
+   
 }
