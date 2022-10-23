4af207bd3bec3ef6e47a43e7b3dde1d8b97c64a1
Nicolas Moreira Nobre Leite <nicolas.leite@ccc.ufcg.edu.br>
2022-10-23 13:55:12 -0300

    Teste para ver se a fatura esta associada a um cliente

--- FiltroDeFaturas/src/tests/FaturaControllerTest.java
+++ FiltroDeFaturas/src/tests/FaturaControllerTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import main.Cliente;
 import main.Fatura;
 import main.FaturaController;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,5 +21,13 @@ public class FaturaControllerTest {
         FaturaController fc = new FaturaController();
         fc.adicionaFatura(new Fatura(7, 1000, "20/10/2022 00:00"));
         assertEquals(1, fc.getListaDeFaturas().size());
+    }
+
+    @Test
+    public void testFaturaEstaAssociadaACliente(){
+        FaturaController fc = new FaturaController();
+        Cliente c = new Cliente("Cliente", "14/04/2010 00:00", "AM");
+        fc.adicionaFatura(new Fatura(1, 1000, "20/10/2022 00:00"));
+        assertTrue(c.checaSeTemFatura(1));
     }
 }
